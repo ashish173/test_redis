d2596d86b0862d41b703e9d501786cd63114f417
Ashish Singh <ashish@aviabird.com>
2022-07-28 18:43:47 +0530

    added stream

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,7 @@
-use std::{io, str};
+use tokio::fs::File;
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufWriter};
+
+use std::{io as Test, str};
 use tokio::io::Interest;
 use tokio::net::{TcpListener, TcpStream, UnixListener, UnixStream};
 
@@ -11,35 +14,50 @@ pub async fn init_connection() {
 
 struct Server {}
 
-fn parse_data(data: &str) {
+async fn parse_data(data: &str, stream: &mut BufWriter<TcpStream>) -> io::Result<()> {
     // get command name (set, get)
+    println!("In Parse Data");
     println!("length {}", data.len());
     let mut buffer = BytesMut::with_capacity(64);
-    let in_bytes = data.as_bytes();
-    buffer.put(&in_bytes[..]);
+    // let in_bytes = data.as_bytes();
+    // let a = "hdhd".to_string();
+    // let b = &b"df"[..];
 
-    println!("Buffer {:?}", buffer);
+    // buffer.put(in_bytes);
+    // // buffer.put_slice(in_bytes);
+
+    println!("Stream {:?}", stream);
+
+    let count = stream.read_buf(&mut buffer).await?;
+
+    println!(" Buffer {:?}", buffer);
+
     // let split: Vec<&str> = data.trim().split(' ').collect();
     // println!("split {:?}", split);
 
     // based on command call the command::apply method
+    Ok(())
 }
 
 async fn process_socket(socket: TcpStream) -> io::Result<()> {
-    println!("socket {:?}", socket);
+    // println!("socket {:?}", socket);
     let server_result = socket.ready(Interest::READABLE).await?;
     println!("{:?}", server_result);
+
     if server_result.is_readable() {
+        println!("value is readable");
         let mut data = vec![0; 1024];
         let val = socket.try_read(&mut data);
-        println!("result:{:?}", val);
-        println!("result:{:?}", data);
+        // println!("result:{:?}", val);
+        // println!("result:{:?}", data);
+
+        let mut stream = BufWriter::new(socket);
 
         let str_data = std::str::from_utf8(&data);
         match str_data {
             Ok(v) => {
-                print!("{}", v);
-                parse_data(v);
+                print!("matched value {}", v);
+                parse_data(v, &mut stream).await?;
             }
             Err(v) => print!("{}", v),
         }
@@ -49,6 +67,17 @@ async fn process_socket(socket: TcpStream) -> io::Result<()> {
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let f = File::create("foo.txt").await?;
+    {
+        let mut writer = BufWriter::new(f);
+
+        // Write a byte to the buffer.
+        writer.write(&[42u8]).await?;
+
+        // Flush the buffer before it goes out of scope.
+        writer.flush().await?;
+    } // Unless flushed or shut down, the contents of the buffer is discarded on drop.
+
     println!("in server");
     // Listen on socket
     let listener = TcpListener::bind("127.0.0.1:8081").await?;
