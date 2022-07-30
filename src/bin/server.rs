1687d7769fdde918bd62af0ab179f8401ca58566
Ashish Singh <ashish@aviabird.com>
2022-07-30 14:23:18 +0530

    command enum and matching

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,11 @@
 use tokio::fs::File;
 use tokio::io::{self, AsyncReadExt, AsyncWriteExt, BufWriter};
+use tokio::time::error::Error;
 
+use std::fmt::{self, Formatter};
+// use std::fmt::Error;
 use std::io::Cursor;
+use std::slice::ChunksExact;
 use std::{io as Test, str};
 use tokio::io::Interest;
 use tokio::net::{TcpListener, TcpStream, UnixListener, UnixStream};
@@ -13,24 +17,66 @@ pub async fn init_connection() {
     // TCPListener
 }
 
- struct Get {
+use core::fmt::Debug;
+pub enum Command {
+    Get,
+    Set,
+    Invalid,
+}
+
+impl Debug for Command {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match *self {
+            Command::Get => write!(f, "Get"),
+            Command::Set => write!(f, "Set"),
+            Command::Invalid => write!(f, "Invalid"),
+        }
+    }
+}
+
+struct Get {
     key: String,
 }
+
+impl Get {
+    pub fn apply() -> Result<&'static str, Error> {
+        let result = "success response";
+        Ok(result)
+    }
+}
+
 struct Set {
     key: String,
     value: String,
 }
- fn get_command(data: &str){
+
+impl Set {
+    pub fn apply(self) -> Result<&'static str, Error> {
+        let result = "success response";
+        Ok(result)
+    }
+}
+fn get_command(data: &str) -> Command {
     match data {
         "set" => {
             println!("Set command matched");
+            Command::Set
+            // let set = Set {
+            //     key: "sdf".to_string(),
+            //     value: "dfdf".to_string(),
+            // };
+            // fetch_attrs()
+            // set.apply();
         }
         "get" => {
+            // let get = Get {
+            //     key: "sdf".to_string(),
+            //     // value: "dfdf".to_string(),
+            // };
             println!("Get command matched");
+            Command::Get
         }
-        _ => {
-            println!("default")
-        }
+        _ => Command::Invalid,
     }
 }
 async fn process_socket(socket: TcpStream) -> io::Result<()> {
@@ -44,9 +90,26 @@ async fn process_socket(socket: TcpStream) -> io::Result<()> {
         let mut chunked_data = data.chunks_exact(3);
         let command = std::str::from_utf8(chunked_data.next().unwrap()).unwrap();
         println!("command:{}", command);
-        get_command(command);
+        let cmd: Command = get_command(command);
+        fetch_attrs(cmd, chunked_data);
     }
     Ok(())
+}
+
+pub fn fetch_attrs(cmd: Command, chunked_data: ChunksExact<u8>) {
+    // match command, Get, Set -> Enum Command
+    println!("in fetch attrs, {:?}", cmd);
+    match cmd {
+        Command::Get => {
+            println!("get");
+        }
+        Command::Set => {
+            println!("set");
+        }
+        Command::Invalid => {
+            println!("invalid");
+        }
+    }
 }
 
 #[tokio::main]
