5aedc3f7d245fb02c35abe9175e7a9ae9d6b8156
Rohit171994 <rohitkumarpoddar1917@gmail.com>
2022-07-30 18:33:51 +0530

    able to read server data

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,6 +1,6 @@
+use bytes::{buf, BytesMut};
 use std::{io, thread, time};
-use bytes::BytesMut;
-use tokio::io::{AsyncWriteExt, Interest};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Interest};
 use tokio::net::TcpStream;
 
 pub fn init_connection() {
@@ -12,22 +12,27 @@ struct Client {}
 #[tokio::main]
 pub async fn main() -> io::Result<()> {
     // init_connection()
-    
-    // let mut data= BytesMut::with_capacity(4 * 1024);
-    let mut data= BytesMut::with_capacity(4 * 1024);
 
+    // let mut data= BytesMut::with_capacity(4 * 1024);
+    let mut data = BytesMut::with_capacity(4 * 1024);
 
     let mut stream = TcpStream::connect("127.0.0.1:8081").await?;
-   
-    stream.writable().await?;
-    stream.write_all(b"set foo ggg").await?;
+
+    // stream.writable().await?;
+    stream.write_all(b"set").await?;
     // println!("Result_new{:?}", stream);
-    thread::sleep(time::Duration::from_millis(1000));
+    // thread::sleep(time::Duration::from_millis(1000));
     let server_result = stream.ready(Interest::READABLE).await?;
 
-    if server_result.is_readable(){
-        stream.try_read(&mut data);
-        println!("Reading the data:==={:?}", std::str::from_utf8(&data));
+    if server_result.is_readable() {
+        stream.read_buf(&mut data).await?;
+        {
+            if let Ok(string) = std::str::from_utf8(&mut data) {
+                println!("\"{}\"", string);
+            } else {
+                println!("{:?}", data);
+            }
+        };
     }
     Ok(())
 }
